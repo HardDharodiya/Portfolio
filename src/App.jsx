f05c7e66a4a15755430cc8e313bc11a55d6c9435
HardDharodiya <dharodiyaneev07@gmail.com>
2025-04-21 08:45:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="overflow-x-hidden">>
+    <div className="overflow-x-hidden">
       <NavBar/>
       <Hero />
       <About />
